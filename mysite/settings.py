ec4367b214b6b66f2dd682a6e81e5fa5f4cc154f
PortalRU <ilnar.16-02@mail.ru>
2021-12-15 17:25:06 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,7 +103,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "static"),
 )
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
