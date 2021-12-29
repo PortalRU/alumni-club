f969baf01a5605a54c81101a6bd384c30988f5e0
PortalRU <ilnar.16-02@mail.ru>
2021-12-29 17:50:02 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -99,8 +99,8 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
