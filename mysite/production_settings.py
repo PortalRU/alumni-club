65b1404daffe68993ec34905af12ab19d66c3c16
PortalRU <ilnar.16-02@mail.ru>
2021-12-29 19:35:04 +0300

    Update production_settings.py

--- mysite/production_settings.py
+++ mysite/production_settings.py
@@ -28,6 +28,28 @@ ALLOWED_HOSTS = (
     'localhost',
 )
 
+ROOT_URLCONF = 'mysite.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(PROJECT_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'mysite.wsgi.application'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 SECURE_SSL_REDIRECT = True
