ef3e5f09dab74bc78d1b5a35c20bac0fe26aa09a
PortalRU <ilnar.16-02@mail.ru>
2021-12-29 18:32:17 +0300

    update

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,9 +1,7 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-if os.getenv("ENV") == "Heroku":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.production_settings')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
